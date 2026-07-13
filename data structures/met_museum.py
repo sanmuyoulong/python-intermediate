0cee35effd3c90b023e4735549e764625448573c
sanmuyoulong <2318390047@qq.com>
2026-07-13 10:10:25 +0800

    met museum

---
+++ data structures/met_museum.py
@@ -0,0 +1,25 @@
+# Instructions
+# As one of the cultural capitals of the world, NYC is the home of the Met Museum. 🏛️
+
+# It has one of the biggest art collections in the world!
+
+# Let’s catalog an artifact from the museum! 🖼️ 👩‍🎨
+
+# First, search the Met Museum site for your favorite artifact. Scroll to "Artwork Details" and let's start cataloging.
+
+# On the Python editor, create a dictionary with the information of your artifact, including:
+
+# artist
+# period
+# date
+# Lastly, print the dictionary. What do you see? What if we just want to print the keys, or the values?
+
+my_artifact = {
+    "artist": "Vincent van Gogh",
+    "period": "Post-Impressionism",
+    "date": "1889"
+}
+
+print(my_artifact)  # Prints the entire dictionary
+print(my_artifact.keys())  # Prints the keys of the dictionary
+print(my_artifact.values())  # Prints the values of the dictionary
