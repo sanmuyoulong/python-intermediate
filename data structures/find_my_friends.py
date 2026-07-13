ea9aa6b476c4bec5d7e688befb10e9d330023dbb
sanmuyoulong <2318390047@qq.com>
2026-07-13 10:01:33 +0800

    find my friends

---
+++ data structures/find_my_friends.py
@@ -0,0 +1,25 @@
+#Instructions
+# Thanks to the internet, we can connect with friends across the world. 🧑‍🤝‍🧑
+
+# When you and your friends are scattered across different cities, sharing locations is something that can help you feel closer. 🌐
+
+# Let's use latitude and longitude to create tuples of our friends locations!
+
+# Create a tuple for the city you are in.
+# Create 3 tuples for your friends, each with the latitude and longitude of their cities.
+# Print the locations of all your friends.
+# Which of your friends is the furthest away?
+
+# Bonus: See if you can combine all the tuples into one tuple.
+
+my_city = (40.7128, -74.0060)  # Example: New York City (latitude, longitude)
+friend1 = (34.0522, -118.2437)  # Example: Los Angeles
+friend2 = (51.5074, -0.1278)    # Example: London
+friend3 = (35.6895, 139.6917)   # Example: Tokyo
+
+combined_locations = (my_city, friend1, friend2, friend3)
+print("My city location:", my_city)
+print("Friend 1 location:", friend1)
+print("Friend 2 location:", friend2)
+print("Friend 3 location:", friend3)
+print("Combined locations:", combined_locations)
